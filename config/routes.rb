8bf9d8b65a7c603e96772c4dd091a09bf10fb644
Roberto Barros <roberto.f.barros@gmail.com>
2024-04-25 22:02:45 -0300

    livecode

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  resources :gardens do
+    resources :plants, only: [:create]
+  end
+
+  resources :plants, only: [:destroy]
+
+
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  root "gardens#index"
+end
---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+Garden.create!(name: 'My Little Garden', image_url: 'https://plus.unsplash.com/premium_photo-1678655852256-5fc5670b83eb')
+Garden.create!(name: 'My Big Garden', image_url: 'https://images.unsplash.com/photo-1594498653385-d5172c532c00')
